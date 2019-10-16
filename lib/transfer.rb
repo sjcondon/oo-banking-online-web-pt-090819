92bf3806a89358f01fb0e70dfa424c1bcd3bcb6f
sarah <s.j.condon01@gmail.com>
2019-10-16 00:05:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,13 +25,14 @@ end
     elsif @sender.balance > @amount && @status == "complete"
     @sender.balance = @sender.balance
     @receiver.balance = @receiver.balance
+  
   else
     @sender.valid? == false && @status == "pending"
     @status = "rejected"
     "Transaction rejected. Please check your account balance."
     end
   end
-end
+
 
 
   def reverse_transfer
